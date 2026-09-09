c31046fefb9e8988cc0e053012c8a1b5f2b98e0e
raulcoronam <raulcorona.ing@gmail.com>
2026-09-09 15:36:30 -0600

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        order = {")":"(", "}":"{", "]":"["}
+        for i in s:
+            n = len(stack)
+            if i == "{" or i == "(" or i == "[":
+                stack.append(i) 
+            elif (n > 0) and (order[i] == stack[-1]): 
+                stack.pop() 
+            else: 
+                return False
+        if stack: 
+                return False
+        else: 
+            return True 
+
+        
+
+"""
+
+Lo último que se abrió debe ser lo primero que se cierre
+
+"""
